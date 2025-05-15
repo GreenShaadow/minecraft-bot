83f3cdf783663b33bd0f82befd97f874bcdc59b8
GreenShaadow <avramovicmateja012@gmail.com>
2025-05-15 20:31:21 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const mineflayer =
+require('mineflayer);
+
+const bot = mineflayer.createBot({
+  host: 'mc1679557.fmcs.cloud',
+  port: 35592,
+  username: 'AfkBot',
+  version: false
+});
+
+bot.on('spawn', () => {
+  console.log('Bot je ušao na server!');
+});
